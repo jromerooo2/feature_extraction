cf2a967c50ae2e1c20e545343965528adbcdc375
jromerooo2 <juan.romeroramos9@gmail.com>
2023-07-13 11:59:42 -0400

    converting to data frame and adding the smiling per frame to pd

--- smile.py
+++ smile.py
@@ -9,27 +9,29 @@ import time
 
 def loop_video(vid_path):
     v= cv2.VideoCapture(vid_path)
-    i=1
+    i=0
+    frames_info = []
     while True:
         ret,frame = v.read()
-        # print(frame)
-        detect_smile(frame,i)
-        i+=1
         if ret == False:
+            print("im bout to break")
             break
+        i+=1
+        c,cs = detect_smile(frame,i)
+        print(ret)
+        frames_info.append([c,cs])
+    return frames_info
 
 def detect_smile(frame=None,i=0):
-
     np.set_printoptions(suppress=True)
     model = load_model("keras_Model.h5", compile=False)
     class_names = open("labels.txt", "r").readlines()
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
     detected_face = DeepFace.detectFace(frame, detector_backend = 'opencv')
-    if i >= 34:
-        plt.imshow(detected_face)
-        plt.title("figure " + str(i))
-        plt.show()
+    # if i >= 34:
+    #     plt.imshow(detected_face)
+    #     plt.title("figure " + str(i))
+    #     plt.show()
     image = Image.fromarray(np.uint8(detected_face*255)).convert("RGB")
     image = image.resize((64,64))
     size = (224, 224)
@@ -45,10 +47,4 @@ def detect_smile(frame=None,i=0):
     index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
-
-    print("Class:", class_name[2:], end="")
-    print("Frame number" + str(i))
-    print("Confidence Score:", confidence_score)
-
-# detect_smile()
-# loop_video("./samples/diego.mp4")
+    return[ class_name[2:] , confidence_score]
--- video.py
+++ video.py
@@ -3,7 +3,7 @@ from feat.utils.io import get_test_data_path
 from feat.plotting import imshow
 import os
 from smile import loop_video
-
+import pandas as pd
 #initialize the detector
 detector = Detector(
     face_model="retinaface",
@@ -13,12 +13,21 @@ detector = Detector(
     facepose_model="img2pose",
 )
 
-img_folder = 'samples'
+img_folder = './samples/'
 all_img = [f for f in os.listdir(img_folder) if f.endswith('.mp4')]
 all_img.sort()
 
 for file in all_img:
-    face_video = os.path.join('samples',file)
+    face_video = os.path.join('./samples/' + file)
+    #returns a dataframe that will be used to save later
     pred = detector.detect_video(face_video)
+    #returns an array of arrays -> [[class_name (smile or no smile),confidence score ]]
+    #idea is to save the new data as new columns in the original dataframe
     smile_frame = loop_video(face_video)
-    print(pred)
+    column = []
+    for array in smile_frame:
+        column.append(array[0])
+    pred = pd.DataFrame(pred)
+    pred["Class"] = column
+
+    # print(pred)
